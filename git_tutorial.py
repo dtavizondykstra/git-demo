47923e9f858aca9e98f4b38460d7dd4e7171d95b
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2024-08-09 17:19:21 -0600

    add docstring to add numbers

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,3 +1,8 @@
 def add_numbers(a, b, c):
     """Return the sum of three numbers."""
     return a + b + c
+
+# 1. extend_add numbers to allow an arbitrary number of parameters
+# 2. investigate and resolve security exploit of add_numbers()
+# 3. make the implementation of add_numbers() more efficient
+# 4. add documentation for add_numbers()
