dc5cdf2a3db77e401495edb4bed1f8e4054d2581
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2024-08-10 02:41:43 -0600

    merge bugfix/performance sum function to add numbers

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,6 +1,6 @@
 def add_numbers(a, b, c):
     """Return the sum of three numbers."""
-    return a + b + c
+     return sum(a, b, c)
 
 # 1. extend_add numbers to allow an arbitrary number of parameters
 # 2. investigate and resolve security exploit of add_numbers()
